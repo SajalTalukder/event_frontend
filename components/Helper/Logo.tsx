b750c55a4b114ce91dacbff452961e291b4f5a0f
Sajal Talukder <sajaltalukder01@gmail.com>
2025-07-11 20:45:08 +0600

    logo fin

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = () => {
       <div className="w-10 h-10 bg-cyan-800 dark:bg-white rounded-full flex items-center justify-center">
         <MdEvent className="w-5 h-5 text-white dark:text-black" />
       </div>
-      <h1 className="text-xl md:text-2xl font-bold text-cyan-800 dark:text-white">
+      <h1 className="text-xl md:text-2xl hidden sm:block font-bold text-cyan-800 dark:text-white">
         EventTvent
       </h1>
     </Link>
